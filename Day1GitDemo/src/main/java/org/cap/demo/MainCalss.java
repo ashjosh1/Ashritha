a9d861c0331e4f1a26dd4c36f111cb1a620396a3
askommar <askommar@DIN73002746.corp.capgemini.com>
2018-08-06 10:35:23 +0530

    done

---
+++ Day1GitDemo/src/main/java/org/cap/demo/MainCalss.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainCalss {
+
+	public static void main(String[] args) {
+		System.out.println("HEllo");
+
+	}
+	public static void greet() {
+		System.out.println("world");
+	}
+
+}
