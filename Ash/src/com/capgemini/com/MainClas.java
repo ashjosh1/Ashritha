4b1049568cca786550558b39a4b0f1fb291dfa97
sangee <sangee@DIN80011914.corp.capgemini.com>
2018-12-12 15:31:33 +0530

    done1

--- Ash/src/com/capgemini/com/MainClas.java
+++ Ash/src/com/capgemini/com/MainClas.java
@@ -3,7 +3,9 @@ package com.capgemini.com;
 public class MainClas {
 
 	public static void main(String[] args) {
-System.out.println("hello");		// TODO Auto-generated method stub
+System.out.println("hello");
+System.out.println("hello");
+// TODO Auto-generated method stub
 
 	}
 
